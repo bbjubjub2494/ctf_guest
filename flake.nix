dfe0ad1db68672fae7a29c7b8130b6ba9470bd7f
Louis Bettens <louis@bettens.info>
2022-07-31 19:43:43 +0200

    add custom dwm

--- flake.nix
+++ flake.nix
@@ -10,7 +10,12 @@
   inputs.miniguest.url = "github:lourkeur/miniguest";
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
   inputs.nixpkgs-21_11.url = "nixpkgs/nixos-21.11";
+  inputs.home-manager.url = "github:nix-community/home-manager";
+  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+  inputs.distro.url = "/home/louis/perso/config";
+  inputs.ate.url = "github:andir/ate";
+  inputs.ate.flake = false;
   inputs.RedNixOS.url = "github:redcode-labs/RedNixOS";
   inputs.rednix.follows = "RedNixOS/rednix";
 
@@ -18,6 +23,7 @@
   outputs = inputs @ {
     self,
     devshell,
+    distro,
     fup,
     miniguest,
     nixpkgs,
@@ -34,45 +40,75 @@
       nixosConfigurations.default =
         nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          modules = [
-            miniguest.nixosModules.core
-            ({pkgs, ...}: {
-                nixpkgs.config.allowUnfree = true;
-                nixpkgs.config.permittedInsecurePackages = [
-                  "tightvnc-1.3.10"
-                ];
-
-                _module.args = {
-                  inherit inputs self;
-                  pkgs21_11 = import inputs.nixpkgs-21_11 {
-                    system = "x86_64-linux";
-                    config.allowUnfree = true;
+          modules =
+            distro.suites.dwm
+            ++ [
+              inputs.home-manager.nixosModules.default
+              miniguest.nixosModules.core
+              ({
+                  lib,
+                  pkgs,
+                  ...
+                }: {
+                  nixpkgs.config.allowUnfree = true;
+                  nixpkgs.config.permittedInsecurePackages = [
+                    "tightvnc-1.3.10"
+                  ];
+                  home-manager.users.red = {
+                    imports = distro.home.suites.dwm;
+                    nixpkgs.overlays = [
+                      (final: _: {
+                        ate = final.callPackage inputs.ate {};
+                      })
+                    ];
+                    programs.dwm.colors = let
+                      coal = "#100c0a";
+                      ruby = "#e31e26";
+                      lightgray = "#fce8c3";
+                      red = "#FF0000";
+                      white = "#FFFFFF";
+                    in {
+                      normfg = lightgray;
+                      normbg = coal;
+                      normborder = coal;
+                      selfg = white;
+                      selbg = ruby;
+                      selborder = lightgray;
+                    };
+                    home.stateVersion = "22.11";
                   };
-                };
-                boot.miniguest.enable = true;
 
-                fileSystems."/" = {
-                  device = "root";
-                  fsType = "tmpfs";
-                  options = ["defaults" "mode=755"];
-                };
+                  _module.args = {
+                    inherit inputs self;
+                    pkgs21_11 = import inputs.nixpkgs-21_11 {
+                      system = "x86_64-linux";
+                      config.allowUnfree = true;
+                    };
+                  };
+                  boot.miniguest.enable = true;
 
-                environment.systemPackages =
-                  [
-                    (pkgs.python3.withPackages (p:
-                      with p; [
-                        pwntools
-                        pycryptodome
-                        gmpy2
-                      ]))
-                  ]
-                  ++ pkgs.lib.attrValues inputs.rednix.packages.${pkgs.system};
+                  fileSystems."/" = {
+                    device = "root";
+                    fsType = "tmpfs";
+                    options = ["defaults" "mode=755"];
+                  };
 
-                users.users.red.openssh.authorizedKeys.keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCNjyVyANhlBmXytKjbXmk+qe+Tb8KoAGAnqjhooE5I4pBZqdEvcWqw+vhi1zUpNFk1EA6c7/czV4rmYgfXzqOVTPcmOPbrnZoAa9et6oOJMYE2RsPMLGCM18kcZHU656xtS+HfrsX+VRnEu/n8Mxx1tOKPe+JM3m1gIan3WEhbaEOlivUeXY3arnjPx1f11WIiZ+ZymBuOYo0yvYAx6FpILcFvdMfWDsiNBWNaOKqMxe12vZ+3JmbEJWioPp+oD6gb6HF4x92jajuG/MwtGkwfaKbOeaUYDSaYezl2vabLSuDhvRzXxhvWmiBjGkEDG4Sf4eRAwZ8XVsI6t9P6sxrL cardno:000500003C7C
+                  environment.systemPackages =
+                    [
+                      (pkgs.python3.withPackages (p:
+                        with p; [
+                          pwntools
+                          pycryptodome
+                          gmpy2
+                        ]))
+                    ]
+                    ++ pkgs.lib.attrValues inputs.rednix.packages.${pkgs.system};
+
+                  users.users.red.openssh.authorizedKeys.keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCNjyVyANhlBmXytKjbXmk+qe+Tb8KoAGAnqjhooE5I4pBZqdEvcWqw+vhi1zUpNFk1EA6c7/czV4rmYgfXzqOVTPcmOPbrnZoAa9et6oOJMYE2RsPMLGCM18kcZHU656xtS+HfrsX+VRnEu/n8Mxx1tOKPe+JM3m1gIan3WEhbaEOlivUeXY3arnjPx1f11WIiZ+ZymBuOYo0yvYAx6FpILcFvdMfWDsiNBWNaOKqMxe12vZ+3JmbEJWioPp+oD6gb6HF4x92jajuG/MwtGkwfaKbOeaUYDSaYezl2vabLSuDhvRzXxhvWmiBjGkEDG4Sf4eRAwZ8XVsI6t9P6sxrL cardno:000500003C7C
 "];
-              })
-            "${RedNixOS.outPath}/rednixos-iso-stable.nix"
-          ];
+                })
+              "${RedNixOS.outPath}/rednixos-iso-stable.nix"
+            ];
         };
 
       outputsBuilder = channels: {
